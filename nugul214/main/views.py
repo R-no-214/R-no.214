4d4cb310100c8063778b6b031874424b867fb49e
soohyunkim <sss4920@likelion.org>
2019-11-29 11:11:04 +0900

    base_dir set in settings.py

--- nugul214/main/views.py
+++ nugul214/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Hello World!')
+def hello(request):
+    return render(request, 'main/hello.html')
 
 # Create your views here.
